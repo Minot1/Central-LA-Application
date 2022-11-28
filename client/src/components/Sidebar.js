6ff10fb479bd37cc5182889f4f9b7e3a205e8569
murat <muratkaraca@sabanciuniv.edu>
2022-11-28 21:07:34 +0300

    modified sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -13,9 +13,17 @@ import CampaignIcon from "@mui/icons-material/Campaign";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
-import { Avatar } from "@mui/material";
+import { Avatar, Collapse } from "@mui/material";
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 function Sidebar() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <Drawer
       open={true}
@@ -53,59 +61,70 @@ function Sidebar() {
             }}
           >
             <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
-            <Avatar sx={{ width: 70, height: 70 }} src={"https://m.media-amazon.com/images/M/MV5BMGIyYjNhOTUtM2UxNC00MjFlLTkwZGYtNDQ0MDhmNGNkYWNlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"}></Avatar>
+              <Avatar sx={{ width: 70, height: 70 }} src={"https://m.media-amazon.com/images/M/MV5BMGIyYjNhOTUtM2UxNC00MjFlLTkwZGYtNDQ0MDhmNGNkYWNlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"}></Avatar>
             </Box>
             <Typography sx={{ textAlign: 'center', padding: "3px" }}>Ad Soyad</Typography>
             <Typography sx={{ textAlign: 'center', padding: "3px" }}>Instructor</Typography>
           </Box>
         </ListItem>
-        <ListItem sx={{ padding: "7px" }}>
-          <ListItemButton sx={{ padding: "0px" }}>
+        <ListItem sx={{ padding: "0px" }}>
+          <ListItemButton>
             <ListItemIcon sx={{ minWidth: "30px" }}>
               <HomeIcon sx={{ color: "white" }} />
             </ListItemIcon>
             <ListItemText primary={"Home"} />
           </ListItemButton>
         </ListItem>
-        <ListItem sx={{ padding: "7px" }}>
+        <ListItem sx={{ padding: "0px" }}>
           <ListItemButton>
-            <ListItemIcon>
+            <ListItemIcon sx={{ minWidth: "30px" }}>
               <SearchIcon sx={{ color: "white" }} />
             </ListItemIcon>
             <ListItemText primary={"Search"} />
           </ListItemButton>
         </ListItem>
-        <ListItem sx={{ padding: "7px" }}>
+        <ListItem sx={{ padding: "0px" }}>
           <ListItemButton>
-            <ListItemIcon>
+            <ListItemIcon sx={{ minWidth: "30px" }}>
               <NotificationsIcon sx={{ color: "white" }} />
             </ListItemIcon>
             <ListItemText primary={"Notifications"} />
           </ListItemButton>
         </ListItem>
-        <Divider sx={{ bgcolor: "white" }} />
-        <ListItem sx={{ padding: "7px" }}>
-          <ListItemButton>
-            <ListItemIcon>
+        <Divider sx={{ bgcolor: "#e0e0e0", margin: "3px" }} />
+        <ListItem sx={{ padding: "0px" }}>
+          <ListItemButton onClick={handleClick}>
+            <ListItemIcon sx={{ minWidth: "30px" }}>
               <CampaignIcon sx={{ color: "white" }} />
             </ListItemIcon>
             <ListItemText primary={"Announcements"} />
+            {open ? <ExpandLess /> : <ExpandMore />}
           </ListItemButton>
         </ListItem>
-        <List>
-          <ListItem sx={{ paddingY: "0px", paddingX: "7px" }}>
-            <ListItemButton>
-              <ListItemText
-                primary={"All Announcements"}
-                sx={{ textAlign: "end" }}
-              />
-            </ListItemButton>
-          </ListItem>
-        </List>
-        <Divider sx={{ bgcolor: "white" }} />
-        <ListItem sx={{ padding: "7px" }}>
+        <Collapse in={open} timeout="auto">
+          <List>
+            <ListItem sx={{ padding: "0px" }}>
+              <ListItemButton>
+                <ListItemText
+                  primary={"All Announcements"}
+                  sx={{ textAlign: "center" }}
+                />
+              </ListItemButton>
+            </ListItem>
+            <ListItem sx={{ padding: "0px" }}>
+              <ListItemButton>
+                <ListItemText
+                  primary={"My Announcements"}
+                  sx={{ textAlign: "center" }}
+                />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Collapse>
+        <Divider sx={{ bgcolor: "#e0e0e0", margin: "3px" }} />
+        <ListItem sx={{ padding: "0px" }}>
           <ListItemButton>
-            <ListItemIcon>
+            <ListItemIcon sx={{ minWidth: "30px" }}>
               <HistoryIcon sx={{ color: "white" }} />
             </ListItemIcon>
             <ListItemText primary={"History"} />
