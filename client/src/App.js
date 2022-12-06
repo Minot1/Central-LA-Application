900ba8be85822ce700e271f72f066520ab66c56e
Minot1 <dincer.taner123@gmail.com>
2022-12-06 11:32:08 +0300

    added redux structure and simple userSlice

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
 import "./App.css";
 import MockCAS from "./pages/MockCAS";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./theme";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<MockCAS></MockCAS>}></Route>
-          {/* <Route path="/instructor" element={<div><Sidebar></Sidebar></div>}></Route> */}
-          <Route
-            path="/instructor"
-            element={
-                <HomePage></HomePage>
-            }
-          ></Route>
-          <Route path="/student" element={<div></div>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<MockCAS></MockCAS>}></Route>
+        {/* <Route path="/instructor" element={<div><Sidebar></Sidebar></div>}></Route> */}
+        <Route path="/instructor" element={<HomePage></HomePage>}></Route>
+        <Route path="/student" element={<div></div>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
