9679ab3385ea72570af1ee881603e88b8f60f161
Minot1 <dincer.taner123@gmail.com>
2023-05-01 12:30:04 +0300

    remote

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -15,56 +15,50 @@ class UserController extends Controller
     function login(Request $req)
     {
         $user = User::where('username', $req->username)->first();
-        if(!$user||($req->password != $user->password))
-        {
-            return ["result"=>"Wrong username or password"];
+        if (!$user || ($req->password != $user->password)) {
+            return ["result" => "Wrong username or password"];
         }
         return $user;
     }
 
-    function serviceValidate(Request $req){
+    function serviceValidate(Request $req)
+    {
         $ticket = $req->input('ticket');
         $serviceUrl = $req->input('serviceUrl');
         $res = Http::get("https://login.sabanciuniv.edu/cas/serviceValidate?service=$serviceUrl&ticket=$ticket");
-        $xml = simplexml_load_string($res, null, null, 'cas', true);
+        $xml = simplexml_load_string($res, null, 0, 'cas', true);
         $json = json_encode($xml);
-        $array = json_decode($json,TRUE);
+        $array = json_decode($json, TRUE);
 
-        if(array_key_exists("authenticationFailure",$array)){
+        if (array_key_exists("authenticationFailure", $array)) {
             return $array;
-        }
-        else{
-            $role = $array["authenticationSuccess"]["attributes"]["ou"][count($array["authenticationSuccess"]["attributes"]["ou"])-1];
-            if($role == 'student'){
-                $result= Student::where('student_username', $array["authenticationSuccess"]["user"])->value('student_username');
+        } else {
+            $role = $array["authenticationSuccess"]["attributes"]["ou"][count($array["authenticationSuccess"]["attributes"]["ou"]) - 1];
+            if ($role == 'student') {
+                $result = Student::where('student_username', $array["authenticationSuccess"]["user"])->value('student_username');
                 $date = date_create()->format('Y-m-d H:i:s');
-                if(!$result)
-                {
-                    $newStudent= new Student;
-                    $newStudent->student_username=$array["authenticationSuccess"]["user"];
-                    $newStudent->name=$array["authenticationSuccess"]["attributes"]["displayName"];
-                    $newStudent->created_at=$date;
-                    $newStudent->updated_at=$date;
+                if (!$result) {
+                    $newStudent = new Student;
+                    $newStudent->student_username = $array["authenticationSuccess"]["user"];
+                    $newStudent->name = $array["authenticationSuccess"]["attributes"]["displayName"];
+                    $newStudent->created_at = $date;
+                    $newStudent->updated_at = $date;
                     $newStudent->save();
+                } else {
+                    $student = Student::where('student_username', $array["authenticationSuccess"]["user"])->update(["updated_at" => $date]);
                 }
-                else{
-                    $student= Student::where('student_username', $array["authenticationSuccess"]["user"])->update(["updated_at"=>$date]);
-                }
-            }
-            else{
-                $result= Instructor::where('instructor_username', $array["authenticationSuccess"]["user"])->value('instructor_username');
+            } else {
+                $result = Instructor::where('instructor_username', $array["authenticationSuccess"]["user"])->value('instructor_username');
                 $date = date_create()->format('Y-m-d H:i:s');
-                if(!$result)
-                {
-                    $newInstructor= new Instructor;
-                    $newInstructor->instructor_username=$array["authenticationSuccess"]["user"];
-                    $newInstructor->name=$array["authenticationSuccess"]["attributes"]["displayName"];
-                    $newInstructor->created_at=$date;
-                    $newInstructor->updated_at=$date;
+                if (!$result) {
+                    $newInstructor = new Instructor;
+                    $newInstructor->instructor_username = $array["authenticationSuccess"]["user"];
+                    $newInstructor->name = $array["authenticationSuccess"]["attributes"]["displayName"];
+                    $newInstructor->created_at = $date;
+                    $newInstructor->updated_at = $date;
                     $newInstructor->save();
-                }
-                else{
-                    $student= Instructor::where('instructor_username', $array["authenticationSuccess"]["user"])->update(["updated_at"=>$date]);
+                } else {
+                    $student = Instructor::where('instructor_username', $array["authenticationSuccess"]["user"])->update(["updated_at" => $date]);
                 }
             }
 
@@ -72,7 +66,7 @@ class UserController extends Controller
                 'role' => $role,
                 'iat' => time(),
                 "exp" => time() + 60 * 60 * 4
-            ], env('JWT_SECRET'),'RS256');
+            ], env('JWT_SECRET'), 'RS256');
             $array["JWT_TOKEN"] = $jwt;
             $array["role"] = $role;
             return $array;
